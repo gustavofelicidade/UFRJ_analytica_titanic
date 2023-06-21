76b381743583f59a1b506d8a56e0dbb8a8bfc2e9
Gustavo Felicidade <gustavosnik95@gmail.com>
2023-06-21 20:31:31 -0300

    setting READ ME  Equipe 2 -> 0.0.3

--- Scripts/main.py
+++ Scripts/main.py
@@ -7,30 +7,37 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger()
 log = logger.info
 
-# Gist URL of the Titanic dataset
-gist_url = "https://gist.githubusercontent.com/balmasi/0e65f72c48f2a3498ceb36ffc216f5eb/raw/fa71405126017e6a37bea592440b4bee94bf7b9e/titanic.csv"
+# Use
+# from main import dataset
+# In your script to read the dataset
 
-try:
-    # Read Titanic dataset from Gist URL
+
+def dataset():
+    gist_url = "https://gist.githubusercontent.com/balmasi/0e65f72c48f2a3498ceb36ffc216f5eb/raw/fa71405126017e6a37bea592440b4bee94bf7b9e/titanic.csv"
     titanic_data = pd.read_csv(urllib.request.urlopen(gist_url))
-
-    # Perform exploratory data analysis
-    log("Data Description:")
-    log(titanic_data.describe())
-
-    log("Column Information:")
-    log(titanic_data.info())
-
-    log("Null Value Counts:")
-    log(titanic_data.isnull().sum())
-
-except Exception as e:
-    log(f"Error occurred: {str(e)}")
+    return titanic_data
 
 
-log("--------------------------------")
-log("------ titanic_data ----------")
-log(titanic_data.head())
+# Comment the description below to omit log messages
+if __name__ == "__main__":
+
+    try:
+        # Read Titanic dataset from Gist URL
+        titanic_data = dataset()
+
+        # Perform exploratory data analysis
+        log("Data Description:")
+        # log(titanic_data.describe())
+
+        log("Column Information:")
+        # log(titanic_data.info())
+
+        log("Null Value Counts:")
+        # log(titanic_data.isnull().sum())
+
+    except Exception as e:
+        log(f"Error occurred: {str(e)}")
 
 
+    log("--------------------------------")
 
